f879f3faf82e5bea5548817aa4e9366194868348
SebastiánMartínez <sebmartinezse@unal.edu.co>
2024-08-31 10:10:29 -0500

    Traduciendo métodos

--- src/gestorAplicacion/administracion/Grupo.py
+++ src/gestorAplicacion/administracion/Grupo.py
@@ -13,3 +13,24 @@ class Grupo:
         self.salon = salon
         self.estudiantes = []
         Grupo.grupos_totales.append(self)
+        
+    def mostrar_informacion_grupo(self):
+        retorno = f'Número del grupo: {self.numero}, Profesor: {self.profesor}, Horario: {self.horario}, Cupos: {self.cupos}, Salon: {self.salon}'
+        return retorno
+    
+    def existencia_estudiante(self, estudiante):
+        for est in self.estudiantes:
+            if est.get_id() == estudiante.get_id():
+                return True
+        return False
+    
+    def eliminar_estudiante(self, estudiante):
+        indice = -1
+        for i, est in enumerate(self.estudiante):
+            if est.get_nombre() == estudiante.get_nombre():
+                indice = i
+                self.cupos += 1
+                estudiante.eliminar_grupo(self)
+                break
+        if indice != -1:
+            self.estudiantes.pop(indice)
